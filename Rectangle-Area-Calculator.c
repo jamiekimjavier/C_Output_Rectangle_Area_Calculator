6d7d1e2f2726f0c750826a49dfe41481611414b7
jamiekimjavier <jamiekimjavier@gmail.com>
2024-10-10 10:36:47 +0800

    Add files via upload

---
+++ Rectangle-Area-Calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    int main(void) {
+
+        const int length = 20;
+        const int width = 40;
+
+        int rectangle = length * width;
+
+        printf("Rectangle Area: %d\n", rectangle);
+
+
+        return 0;
+    }
